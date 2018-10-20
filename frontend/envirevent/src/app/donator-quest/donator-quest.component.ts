baed7ab5d6ba54d4e3e7153c84d6a47fa05346e2
Ashley Lobo <ashleyl@rediffmail.com>
2018-10-20 23:17:07 +0530

    data added

--- frontend/envirevent/src/app/donator-quest/donator-quest.component.ts
+++ frontend/envirevent/src/app/donator-quest/donator-quest.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-donator-quest',
   templateUrl: './donator-quest.component.html',
   styleUrls: ['./donator-quest.component.css']
 })
 export class DonatorQuestComponent implements OnInit {
+  constructor(private data:DataService) { 
 
-  constructor() { }
-
+  }
   ngOnInit() {
   }
 
