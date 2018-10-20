83511c3dec51bf7b149c7ec20f4715e46ff073b3
Ashley Lobo <ashleyl@rediffmail.com>
2018-10-20 13:40:47 +0530

    1

--- frontend/envirevent/src/app/app-routing.module.ts
+++ frontend/envirevent/src/app/app-routing.module.ts
@@ -3,6 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule,ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DonatorComponent } from './donator/donator.component';
+import { CollectorComponent } from './collector/collector.component';
+import { DonatorDonateComponent } from './donator-donate/donator-donate.component';
+import { DonatorTransactionComponent } from './donator-transaction/donator-transaction.component';
+import { DonatorWalletComponent } from './donator-wallet/donator-wallet.component';
+import { DonatorQuestComponent } from './donator-quest/donator-quest.component';
+
 export const routes: Routes = [
   {
     path: '',
@@ -12,6 +19,20 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'donor',
+    component: DonatorComponent,
+    // children: [
+    //   {
+    //     path: '',
+    //     component: DonatorComponent,
+    //   },
+    //   {
+    //     path: '',
+    //     component: DonatorComponent,
+    //   },
+    // ]
   },
 
 ];
