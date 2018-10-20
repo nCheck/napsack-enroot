391ff1588631b8b710eb2cf21d612bd778a323f2
Ashley Lobo <ashleyl@rediffmail.com>
2018-10-20 17:45:57 +0530

    1

---
+++ frontend/envirevent/src/app/data.service.ts
@@ -0,0 +1,12 @@
+import { Observable } from 'rxjs';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {RequestOptions, Request, RequestMethod , Headers} from '@angular/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  options = { headers: this.headers };
+  constructor() { }
+}
